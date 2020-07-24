a00343bbfcfcaf5ef790323fc6a199ce0edd28da
arahansa <arahansa@naver.com>
2020-07-24 23:09:21 +0900

    add first git

---
+++ src/main/java/theater/Ticket.java
@@ -0,0 +1,8 @@
+package theater;
+
+public class Ticket {
+    private Long fee;
+    public Long getFee(){
+        return this.fee;
+    }
+}
---
+++ src/main/java/theater/TicketSeller.java
@@ -0,0 +1,16 @@
+package theater;
+
+public class TicketSeller {
+
+    private TicketOffice ticketOffice;
+
+    public TicketSeller(TicketOffice ticketOffice){
+        this.ticketOffice = ticketOffice;
+    }
+
+    public TicketOffice getTicketOffice(){
+        return ticketOffice;
+    }
+
+
+}
